a3fd40604e602862d0d6550beb329f23b2867fe2
Dustin Kirkland <dustin.kirkland@gmail.com>
2008-03-03 22:41:21 -0600

    added EDIT_PASSWORD variable

--- settings.php
+++ settings.php
@@ -4,5 +4,6 @@ $TITLE = "Tux's Photo Collection";
 $PHOTOGRAPHER = "Tux Himself";
 $PHOTOGRAPHER_EMAIL = "tux@example.com";
 $RAND_ALBUMS = array("pictures/Wedding", "pictures/Birthdays");
+$EDIT_PW = "s0me_passw0rd";
 
 ?>
