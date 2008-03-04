0a11ec00fc691f89d7e9bbbfc4289a89fd5be92c
Dustin Kirkland <dustin.kirkland@gmail.com>
2008-03-03 22:11:19 -0600

    Initial revision of license, settings, and codes files

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$TITLE = "pictures.dustinkirkland.com";
+$PHOTOGRAPHER = "Dustin Kirkland";
+$PHOTOGRAPHER_EMAIL = "dustin.kirkland@gmail.com";
+$RAND_ALBUMS = array("pictures/Weddings/Kirkland-Gros/Rehearsal/slideshow");
+
+?>
