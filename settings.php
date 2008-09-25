63b807774b6c13e738117a202be6bc1b7ef96e1d
Dustin Kirkland <dustin.kirkland@gmail.com>
2008-09-25 10:26:41 -0500

    back to original

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@
 $TITLE = "Tux's Photo Collection";
 $PHOTOGRAPHER = "Tux Himself";
 $PHOTOGRAPHER_EMAIL = "tux@example.com";
-$RAND_ALBUMS = array("pictures/Wedding", "pictures/Birthdays");
+$RAND_ALBUMS = array();
 $EDIT_PW = "s0me_passw0rd";
 $COLUMNS = 6;
 $THUMB_COLUMNS = 4;
